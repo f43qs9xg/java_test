9d8e1e866212afd1e387b0f43a8391b1a6be0f0f
akiyama <kamome730@gmail.com>
2020-05-20 07:49:34 +0900

    miss sample2

--- Main.java
+++ Main.java
@@ -1,56 +1,28 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Main {
-    public static void main(String[] args){
-        // 10の要素を持つ配列
-        int[] x = {5, 10, 15, 20, 7, 18, 4, 1, 25, 19};
-        int[] index = {0,1,2,3,4,5,6,7,8,9};
 
-        Set<String> colors = new HashSet<String>();
+	public static void main(String[] args) {
+		// 出力のフォーマット
+		SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd");
+		Date now = new Date();
+		Calendar c = Calendar.getInstance();
 
-        bubbleSort(x, index);
+		c.setTime(now);
+		// 1日にセット
+		c.set(Calendar.DAY_OF_MONTH, 1);
 
-        System.out.println("昇順ソート後");
-        for(int i : x)System.out.print(i+" ");
+		// 来月の値
+		int nextMonth = c.get(Calendar.MONTH) + 1;
 
-        System.out.println(""); // 改行
+		// 来月になったらループを抜ける
+		while (c.get(Calendar.MONTH) != nextMonth) {
+			Date d = c.getTime();
+			System.out.println(f.format(d));
+			c.add(Calendar.DAY_OF_MONTH, 1);
+		}
+	}
 
-        System.out.println("インデックス順に表示");
-        int indexNum = 0; // 検索するインデックス番号を格納
-
-        for(int i = 0; i < index.length; i++){
-            // 0から順番にインデックスの検索
-            for(int j = 0; i < index.length; j++){
-                if( i == index[j] ){
-                    indexNum = j;
-                    break;
-                }
-            }
-            System.out.print(x[indexNum] + " ");
-        }
-
-    }
-
-    // インデックスを保持したバブルソートを実行
-    public static void bubbleSort(int[] x, int[] index){
-        int index_tmp;
-        int tmp;
-
-        for(int i = x.length -1; i>0; i-- ){
-            for(int j = 0; j < i; j++){
-                // 入れ替え処理
-                if(x[j] > x[j+1]){
-                    index_tmp = index[j+1];
-                    index[j+1] = index[j];
-                    index[j] = index_tmp;
-                    
-                    tmp = x[j];
-                    x[j] = x[j+1];
-                    x[j+1] = tmp;
-                }
-            }
-        }
-
-    }
 }
