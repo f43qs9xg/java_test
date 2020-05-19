41d4c1e4bd53636b50d5193ab04719cb68e75bab
akiyama <kamome730@gmail.com>
2020-05-20 02:10:14 +0900

    miss sample

--- Main.java
+++ Main.java
@@ -1,15 +1,51 @@
-import java.util.HashSet;
-import java.util.Set;
-
-public class Main{
+public class Main {
     public static void main(String[] args){
-        Set<String> colors = new HashSet<String>();
+        // 10の要素を持つ配列
+        int[] x = {5, 10, 15, 20, 7, 18, 4, 1, 25, 19};
+        int[] index = {0,1,2,3,4,5,6,7,8,9};
 
-        colors.add("赤");
-        colors.add("青");
-        colors.add("黄");
-        colors.add("赤";
+        bubbleSort(x, index);
 
-        System.out.println("色は" + colors.size()+ "種類");
+        System.out.println("昇順ソート後");
+        for(int i : x)System.out.print(i+" ");
+
+        System.out.println(""); // 改行
+
+        System.out.println("インデックス順に表示");
+        int indexNum = 0; // 検索するインデックス番号を格納
+
+        for(int i = 0; i < index.length; i++){
+            // 0から順番にインデックスの検索
+            for(int j = 0; i < index.length; j++){
+                if( i == index[j] ){
+                    indexNum = j;
+                    break;
+                }
+            }
+            System.out.print(x[indexNum] + " ");
+        }
+
+    }
+
+    // インデックスを保持したバブルソートを実行
+    public static void bubbleSort(int[] x, int[] index){
+        int index_tmp;
+        int tmp;
+
+        for(int i = x.length -1; i>0; i-- ){
+            for(int j = 0; j < i; j++){
+                // 入れ替え処理
+                if(x[j] > x[j+1]){
+                    index_tmp = index[j+1];
+                    index[j+1] = index[j];
+                    index[j] = index_tmp;
+                    
+                    tmp = x[j];
+                    x[j] = x[j+1];
+                    x[j+1] = tmp;
+                }
+            }
+        }
+
     }
 }
