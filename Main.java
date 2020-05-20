fe6e5b818db2284c7d6d19cc39c9235c579025cb
akiyama <kamome730@gmail.com>
2020-05-21 00:58:18 +0900

    now time

--- Main.java
+++ Main.java
@@ -1,28 +1,16 @@
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// 出力のフォーマット
-		SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd");
-		Date now = new Date();
-		Calendar c = Calendar.getInstance();
+        SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:sss");
+        // 現在の日時を取得
+        Date now = new Date();
+        String s = f.format(now);
 
-		c.setTime(now);
-		// 1日にセット
-		c.set(Calendar.DAY_OF_MONTH, 1);
-
-		// 来月の値
-		int nextMonth = c.get(Calendar.MONTH) + 1;
-
-		// 来月になったらループを抜ける
-		while (c.get(Calendar.MONTH) != nextMonth) {
-			Date d = c.getTime();
-			System.out.println(f.format(d));
-			c.add(Calendar.DAY_OF_MONTH, 1);
-		}
+        System.out.println("現在は"+s+"です");
 	}
 
 }
