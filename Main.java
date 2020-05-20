dfb76916c54d31519bae5dac02af1ab716c22c8d
akiyama <kamome730@gmail.com>
2020-05-21 00:59:49 +0900

    fix tab

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.Date;
 public class Main {
 
 	public static void main(String[] args) {
-		// 出力のフォーマット
+        // 出力のフォーマット
         SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:sss");
         // 現在の日時を取得
         Date now = new Date();
